5a6f74eac4beabb0d4e3a0eff1465c07bdfcb046
Sandeep Mutnal <sandeepmutnal242@gmail.com>
2026-02-19 22:44:34 +0530

    03 JavaScript: variables and differences

---
+++ fundamentals/JavaScript/03_Variables/script.js
@@ -0,0 +1,28 @@
+// Day 03 - Variables
+
+// var example
+var name = "Sandeep";
+console.log("Name:", name);
+
+// let example
+let age = 21;
+console.log("Age:", age);
+
+age = 22; // reassign allowed
+console.log("Updated Age:", age);
+
+// const example
+const country = "India";
+console.log("Country:", country);
+
+// country = "USA"; // ❌ Error
+
+// Difference demo
+var x = 10;
+var x = 20; // allowed
+
+let y = 10;
+// let y = 20; // ❌ not allowed
+
+const z = 30;
+// z = 40; // ❌ not allowed
