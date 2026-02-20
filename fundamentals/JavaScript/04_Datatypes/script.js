21c324b452cf2c88e9927d36d2370a0172e7cf46
Sandeep Mutnal <sandeepmutnal242@gmail.com>
2026-02-20 22:46:59 +0530

    04 JavaScript: Datatypes

---
+++ fundamentals/JavaScript/04_Datatypes/script.js
@@ -0,0 +1,35 @@
+// Day 4: Data Types
+
+// String
+let name = "Vinsan";
+
+// Number
+let age = 21;
+
+// Boolean
+let isStudent = true;
+
+// Null
+let futureGoal = null;
+
+// Undefined
+let marks;
+
+// Object
+let person = {
+  name: name,
+  age: age,
+  isStudent: isStudent
+};
+
+// Array
+let subjects = ["Math", "Science", "English"];
+
+// Output
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Student:", isStudent);
+console.log("Future Goal:", futureGoal);
+console.log("Marks:", marks);
+console.log("Person:", person);
+console.log("Subjects:", subjects);
