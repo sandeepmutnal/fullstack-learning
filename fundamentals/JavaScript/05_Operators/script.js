bf4e555c595f1a955a386ecbeeb55eb83c9b3a89
Sandeep Mutnal <sandeepmutnal242@gmail.com>
2026-02-21 20:22:27 +0530

    05: JavaScript Operators

---
+++ fundamentals/JavaScript/05_Operators/script.js
@@ -0,0 +1,31 @@
+// Day 5: Operators Demo
+
+let a = 20;
+let b = 10;
+
+// Arithmetic
+console.log("Add:", a + b);
+console.log("Subtract:", a - b);
+console.log("Multiply:", a * b);
+console.log("Divide:", a / b);
+console.log("Remainder:", a % b);
+
+// Comparison
+console.log("a > b:", a > b);
+console.log("a == b:", a == b);
+console.log("a === b:", a === b);
+
+// Logical
+let age = 22;
+let hasLicense = true;
+
+console.log("Can Drive:", age >= 18 && hasLicense);
+
+// Assignment
+let x = 5;
+x += 10;
+console.log("x:", x);
+
+// Ternary
+let result = (age >= 18) ? "Eligible to Vote" : "Not Eligible";
+console.log(result);
