df7052abbbf367900f22a515672a1144a400098f
Sandeep Mutnal <sandeepmutnal242@gmail.com>
2026-02-26 22:11:38 +0530

    10 JavaScript: Strings and methods

---
+++ fundamentals/JavaScript/10_Strings/script.js
@@ -0,0 +1,15 @@
+// Template Literals
+let name = "Sandeep";
+console.log(`Hello ${name}`);
+
+// toUpperCase
+let text = "hello world";
+console.log(text.toUpperCase());
+
+// slice
+let word = "JavaScript";
+console.log(word.slice(0, 4));
+
+// includes
+let sentence = "I love JavaScript";
+console.log(sentence.includes("love"));
